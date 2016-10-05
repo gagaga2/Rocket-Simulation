37fa7de28e90a718d16c8f28591fc04671163a72
Max Gabrielsson <max@gabrielsson.com>
2016-10-05 10:11:17 +0200

    Add files via upload
    First upload

---
+++ Rocket/Rocket/Rocket.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace Rocket
+{
+
+    class Rocket
+    {
+        private Texture2D rocket;
+
+        Vector2 movement = new Vector2(0, 0);
+        Vector2 coords = new Vector2(300, 300);
+        float scale = 1f;
+
+        int rocketArea;         //Used for Air Resistance calculations
+        float mass;             //Total mass of rocket
+        float fuelcapacity;     //85% of rocket mass
+        float fuel;             //Remaining fuel
+        float engineEfficiency; // Kn thrust/liter fuel
+
+        int rotation;           //Rotation of rockets nose relative to earth (or closest body??)
+        float altitude;         //Distance from earths sealevel
+
+        public void Load(Texture2D tex)
+        {
+            rocket = tex;
+        }
+
+        public void CalcGravity(Planet[] bodies)
+        {
+            foreach (Planet b in bodies)
+            {
+                //calculate gravity based on distance for all celestial bodies
+            }
+        }
+
+
+
+        public void Update()
+        {
+            KeyboardState state = Keyboard.GetState();
+            if (state.IsKeyDown(Keys.Left))
+            {
+                movement.X -= 2;
+            }
+
+            if (state.IsKeyDown(Keys.Right))
+            {
+                movement.X += 2;
+            }
+            if (state.IsKeyDown(Keys.Down))
+            {
+                movement.Y += 2;
+            }
+            if (state.IsKeyDown(Keys.Up))
+            {
+                movement.Y -= 2;
+            }
+
+            //Slowwly soften acceleration
+            if (movement.X < 0)
+                movement.X += 1;
+
+            if (movement.X > 0)
+                movement.X -= 1;
+
+            if (movement.Y < 0)
+                movement.Y += 1;
+
+            if (movement.Y > 0)
+                movement.Y -= 1;
+
+            //add movement
+            coords += movement;
+
+            if (movement.Y > 3)
+                scale = 0.5f;
+            else
+                scale = 1f;
+        }
+
+        public void Draw(SpriteBatch spritebatch)
+        {
+            spritebatch.Draw(rocket, coords, null, Color.White, 0f, Vector2.Zero, scale, SpriteEffects.None, 0f);
+        }
+
+    }
+}
---
+++ Rocket/Rocket/UniverseManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rocket
+{
+    class UniverseManager
+    {
+
+        Planet[] bodies;
+
+        public UniverseManager() { }
+
+        public void CreateUniverse()
+        {
+            bodies[0] = new Planet("Earth", 2000, 100);
+            bodies[1] = new Planet("Moon", 500, 50);
+        }
+
+        public void ApplyGravitationalPull(Rocket r)
+        {
+            foreach (Planet p in bodies)
+            {
+                //distanceFrom(r) * p.mass
+            }
+        }
+
+        public void Draw()
+        {
+            foreach(Planet p in bodies)
+            {
+                //rita
+            }
+        }
+    }
+}
